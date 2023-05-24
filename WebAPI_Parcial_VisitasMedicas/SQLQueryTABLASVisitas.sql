9b80a35c9fb85032c219998bc8918c8ddb49200a
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-05-24 01:07:11 -0300

    Last Update

--- WebAPI_Parcial_VisitasMedicas/SQLQueryTABLASVisitas.sql
+++ WebAPI_Parcial_VisitasMedicas/SQLQueryTABLASVisitas.sql
@@ -1,3 +1,5 @@
-INSERT INTO dbo.Ambulancias(Id, Empresa, Codigo, ObraSocial, Afiliado, DNI, Precio) VALUES (1,'Vittal', 'Rojo', 'OSDE','Juan Perez', 35055290, 50000);
-INSERT INTO dbo.Ambulancias(Id, Empresa, Codigo, ObraSocial, Afiliado, DNI, Precio) VALUES (2,'Acudir', 'Amarillo', 'OSPAÑA','Rogelio Aguas', 23650667, 32500);
-INSERT INTO dbo.Ambulancias(Id, Empresa, Codigo, ObraSocial, Afiliado, DNI, Precio) VALUES (3,'Pasteur', 'Verde', 'Galeno', 'Pedro Rojas', 3161566, 15000);
+INSERT INTO dbo.Ambulancias( Empresa, Codigo, ObraSocial, Afiliado, DNI, Precio) VALUES ('Vittal', 'Rojo', 'OSDE','Juan Perez', 35055290, 50000);
+INSERT INTO dbo.Ambulancias( Empresa, Codigo, ObraSocial, Afiliado, DNI, Precio) VALUES ('Acudir', 'Amarillo', 'OSPAÑA','Rogelio Aguas', 23650667, 32500);
+INSERT INTO dbo.Ambulancias( Empresa, Codigo, ObraSocial, Afiliado, DNI, Precio) VALUES ('Pasteur', 'Verde', 'Galeno', 'Pedro Rojas', 3161566, 15000);
+
+select * from dbo.Ambulancias
