a90d5b4e349fc0bbe2cc2feb78641c40fb0e7470
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-05-23 22:05:12 -0300

    Update instalacion de nuguets models y controller

---
+++ WebAPI_Parcial_VisitasMedicas/Controllers/AmbulanciaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Parcial_VisitasMedicas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AmbulanciaController : ControllerBase
+    {
+
+    }
+}
---
+++ WebAPI_Parcial_VisitasMedicas/Models/Ambulancia.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_Parcial_VisitasMedicas.Models
+{
+    public class Ambulancia
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "varchar(30)")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Empresa { get; set; }
+        [Column(TypeName = "varchar(30)")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Codigo { get; set; }
+        [Column(TypeName = "varchar(30)")]
+        [Required(ErrorMessage = "El código es obligatorio")]
+        public string ObraSocial { get; set; }
+        [Column(TypeName = "varchar(30)")]
+        [Required(ErrorMessage = "El código es obligatorio")]
+        public string Afiliado { get; set; }
+        [Required(ErrorMessage = "Field required!")]
+        public int DNI { get; set; }
+        [Required(ErrorMessage = "Field required!")]
+        public int Precio { get; set; }
+    }
+}
